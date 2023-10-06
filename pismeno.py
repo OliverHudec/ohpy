eea79eb19af25be67b6c84b955ec36e06dcc6ef4
Ol1kk <65157409+OliverHudec@users.noreply.github.com>
2023-10-06 23:08:46 +0200

    Letter finder

---
+++ pismeno.py
@@ -0,0 +1,9 @@
+slovo = (input(" Zadej libovolné slovo: "))
+
+if 'a' and 'b' in slovo:
+    print(f" Ve slově {slovo} je A i B.")
+elif 'b' in slovo:
+    print(f"Ve slově {slovo} je B.")
+
+elif 'a' in slovo:
+    print(f"Ve slově {slovo} je A.")
